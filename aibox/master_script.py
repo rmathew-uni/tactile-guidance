fa83f3b142ffacd694d40523fc00d37e3e43a170
MiladRouygari <miladrouygari@gmail.com>
2023-09-28 14:20:43 +0200

    two networks run

--- aibox/master_script.py
+++ aibox/master_script.py
@@ -2,8 +2,10 @@ import detect_script_run
 import box_to_FourDirection
 
 if __name__ == '__main__':
-    weights = 'yolov5s.pt'  # Model weights path
+    weights_obj = 'yolov5s.pt'  # Model weights path
+    weights_hand = 'hand_network.pt'
     source = '0'  # Input image path
     # Add other parameters as needed
 
-    detect_script_run.main(weights, source)
+    detect_script_run.main(weights_obj, weights_hand, source)
+    
