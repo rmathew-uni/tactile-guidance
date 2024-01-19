c359bff94b8e74abbc6128d8e199e727e3eb33aa
pippowell <powellpipern@gmail.com>
2024-01-19 10:53:48 +0100

    original exp code

--- flobox/Experiment/tactile_exp_original.py
+++ flobox/Experiment/tactile_exp_original.py
@@ -1,6 +1,11 @@
+# Code modified from original created by Ramon Zacharias
+# Original code from RZ can be found at https://github.com/R3yman/TactileBraceletExperiment
+
+# Import necessary libraries and modules
 import csv
 import random
 import time
+from pathlib import Path
 from random import randrange
 
 import keyboard
@@ -10,15 +15,18 @@ from pybelt.belt_controller import (BeltConnectionState, BeltController,
                                     BeltOrientationType,
                                     BeltVibrationTimerOption)
 
-from aibox.auto_connect import interactive_belt_connect, setup_logger
+from connect import interactive_belt_connect, setup_logger
 
 import os.path   
 import numpy as np
 
+
+# a delegate class for the BeltController
 class Delegate(BeltControllerDelegate):
     # Belt controller delegate
     pass
 
+# Initialize pygame, set random seed, and define Global variables
 pygame.init()
 random.seed()
 
@@ -33,11 +41,12 @@ obs_grasping_t = [["time", "num_instructions", "location", "block", "condition",
 obs_grasping_a = [["time", "num_instructions", "location", "block", "condition", "success"]]
 vibration_intensity = 100
 
+
+# the main function
 def main():
 
     print("Welcome to the \"Augmenting functional vision using tactile guidance\" experiment!")
     print("Please connect the tactile bracelet and press Enter to continue.")
-
 
     # Wait for user to continue.
     while True:
@@ -63,7 +72,7 @@ def main():
         
     else:
         # list of numbers repeated 8 times
-        numbers_t =  [i for i in range(1,10)] * 8
+        numbers_t = [i for i in range(1, 10)] * 8
         # shuffle the list
         random.shuffle(numbers_t)
         # split the shuffled list into 8 block 
@@ -78,7 +87,7 @@ def main():
 
     else:
         # list of numbers repeated 8 times
-        numbers_a =  [i for i in range(1,10)] * 8
+        numbers_a = [i for i in range(1, 10)] * 8
         # shuffle the list
         random.shuffle(numbers_a)
         # split the shuffled list into 8 block 
@@ -87,9 +96,9 @@ def main():
 
     coinflip = random.sample([0,1], 1)[0]
 
-    if coinflip==0:
+    if coinflip == 0:
         print("Condition: Tactile")
-    elif coinflip==1:
+    elif coinflip == 1:
         print("Condition: Auditory")
 
     print("Press 1 to start the localization task. Press 2 to start the grasping task.")
@@ -121,7 +130,9 @@ def main():
                 write_to_csv(participantID, obs_grasping_a, "grasping_a")
             elif user_in == 0:
                 continue
-        
+
+
+# the localization task
 def localization_task():
     while belt_controller.get_connection_state() == BeltConnectionState.CONNECTED:
         print("Q. to quit.")
@@ -139,41 +150,40 @@ def localization_task():
                 stop = False
                 while not stop:
                     print("Current block is number " + str(block_localization))
-                    stimuli = [45,45,45,45,60,60,60,60,90,90,90,90,120,120,120,120] # List of orientations
+                    stimuli = [45, 45, 45, 45,
+                               60, 60, 60, 60,
+                               90, 90, 90, 90,
+                               120, 120, 120, 120]  # List of orientations
 
-                    random.shuffle(stimuli) # Shuffle orientations
+                    random.shuffle(stimuli)  # Shuffle orientations
                     obs_localization.append([])
-                    trans_stimuli = [] # List with stimuli translated from orientation to direction.
+                    trans_stimuli = []  # List with stimuli translated from orientation to direction.
                     # Translate stimuli from orientation to direction.
                     for stimulus in stimuli:
                         if stimulus == 45: trans_stimuli.append("left")
                         elif stimulus == 60: trans_stimuli.append("down")
-                        elif stimulus ==  90: trans_stimuli.append("up")
+                        elif stimulus == 90: trans_stimuli.append("up")
                         elif stimulus == 120: trans_stimuli.append("right")
                     presented_stimuli_localization.append(trans_stimuli)
-                
-                    #print(stimuli)
 
                     time.sleep(3)
                     for stimulus in stimuli:
-                        #print(stimulus)
                         belt_controller.send_pulse_command(
-                        channel_index=0,
-                        orientation_type=BeltOrientationType.ANGLE,
-                        orientation=stimulus,
-                        intensity=vibration_intensity,
-                        on_duration_ms=500,
-                        pulse_period=2000,
-                        pulse_iterations=1,
-                        series_period=1500,
-                        series_iterations=1,
-                        timer_option=BeltVibrationTimerOption.RESET_TIMER,
-                        exclusive_channel=False,
-                        clear_other_channels=False
+                            channel_index=0,
+                            orientation_type=BeltOrientationType.ANGLE,
+                            orientation=stimulus,
+                            intensity=vibration_intensity,
+                            on_duration_ms=500,
+                            pulse_period=2000,
+                            pulse_iterations=1,
+                            series_period=1500,
+                            series_iterations=1,
+                            timer_option=BeltVibrationTimerOption.RESET_TIMER,
+                            exclusive_channel=False,
+                            clear_other_channels=False
                         )
                         obs_localization[block_localization-1].append(collect_response())
-                        #time.sleep(3)
-                    
+
                     user_in = get_input("Block " + str(block_localization) + " completed. Continue?[y,n]", type_=str, range_=["y","n"])
                     block_localization += 1
                     if user_in == "n": stop = True 
@@ -182,7 +192,7 @@ def localization_task():
 
             # Present example stimuli.
             elif user_in == "1":
-                #Up
+                # Up
                 belt_controller.send_pulse_command(
                     channel_index=0,
                     orientation_type=BeltOrientationType.ANGLE,
@@ -198,7 +208,7 @@ def localization_task():
                     clear_other_channels=False
                 )
                 time.sleep(3)
-                #Right
+                # Right
                 belt_controller.send_pulse_command(
                     channel_index=0,
                     orientation_type=BeltOrientationType.ANGLE,
@@ -214,7 +224,7 @@ def localization_task():
                     clear_other_channels=False
                 )
                 time.sleep(3)
-                #Down
+                # Down
                 belt_controller.send_pulse_command(
                     channel_index=0,
                     orientation_type=BeltOrientationType.ANGLE,
@@ -230,7 +240,7 @@ def localization_task():
                     clear_other_channels=False
                 )
                 time.sleep(3)
-                #Left
+                # Left
                 belt_controller.send_pulse_command(
                     channel_index=0,
                     orientation_type=BeltOrientationType.ANGLE,
@@ -247,13 +257,13 @@ def localization_task():
                 )
                 time.sleep(3)
                 break
-            #calc_accuracy()
             # Quit the task.
             if user_in.lower() == "q":
                 return
 
-def grasping_task(condition,prev):
 
+# the grasping task
+def grasping_task(condition, prev):
     new_trial = True
     num_instructions = 0
     time_limit = 20
@@ -262,18 +272,12 @@ def grasping_task(condition,prev):
     begin = 0
     target_idx = 0
     rep_idx = 0
-    #target_list = list(range(1,10))
-
-    # print(targets_t)
-    # print(targets_a)
-    #target_list = [1,2,3,4,5,6,7,8,9]
-    #random.shuffle(target_list)
     rep_list = []
     time_limit_reached = False
 
     # Present example stimuli.
     if condition == "tactile":
-        user_in = get_input("Present example stimuli? [y,n]", range_=("y","n"))
+        user_in = get_input("Present example stimuli? [y,n]", range_=("y", "n"))
         if user_in == "y":
             present_example_stimuli("tactile")
 
@@ -282,8 +286,8 @@ def grasping_task(condition,prev):
         print("The previous block number was " + str(prev))
         block_tactile = get_input("Enter a block number or press 0 to quit the task.", type_=int, min_=0)
         prev = block_tactile
-        target_list = targets_t[block_tactile -1]
-        if block_tactile == 0: # Quit if user enters 0.
+        target_list = targets_t[block_tactile - 1]
+        if block_tactile == 0:  # Quit if user enters 0.
             return
         
     if condition == "auditory":
@@ -291,11 +295,10 @@ def grasping_task(condition,prev):
         block_auditory = get_input("Enter a block number or press 0 to quit the task.", type_=int, min_=0)
         prev = block_auditory
         target_list = targets_a[block_auditory -1]
-        if block_auditory == 0: # Quit if user enters 0.
+        if block_auditory == 0:  # Quit if user enters 0.
             return
     
     # Print targets.
-    
     print("Target order: " + str(target_list))
     print("Target: " + str(target_list[target_idx]))
 
@@ -303,25 +306,23 @@ def grasping_task(condition,prev):
         # Check for start of trial.
         if (keyboard.is_pressed("left") or keyboard.is_pressed("right") or keyboard.is_pressed("up") or keyboard.is_pressed("down") or keyboard.is_pressed("g")) and new_trial and not keyboard.is_pressed("s"):
             begin = time.perf_counter()
-            #print(begin)
-            #num_instructions += 1
             new_trial = False
         
         # Quit the task.
         if keyboard.is_pressed("q"):
             if condition == "tactile":
                 belt_controller.stop_vibration()
-            return #user_in
+            return
         
         # Check time limit.
-        if(begin != 0):
+        if begin != 0:
             if(time.perf_counter() - begin > time_limit) and not new_trial:
                 time_limit_reached = True
         
         # Stop the trial, calculate the time and append the observations.
         if (keyboard.is_pressed("s") or (time_limit_reached)) and not new_trial:
             if condition == "tactile":
-                belt_controller.stop_vibration() 
+                belt_controller.stop_vibration()
 
             if time_limit_reached:
                 elapsed = time_limit
@@ -333,14 +334,14 @@ def grasping_task(condition,prev):
                 elapsed = end - begin # Calculate trial time.
                 print("Trial completed.")
                 print("Completion time is ", elapsed, "seconds.")
-                result = get_input("Press s for success, d for fail or f for experimenter fail.[s,d,f]", type_=str, range_=("s","d","f"))
+                result = get_input("Press s for success, d for fail or f for experimenter fail.[s,d,f]", type_=str, range_=("s", "d", "f"))
                 if result == "s": result = "success"
                 elif result == "d": result = "fail"
-                # Experimenter fail, append target to repetion list.
+                # Experimenter fail, append target to repetition list.
                 else: 
                     result = "exFail"
-                    if target_idx < len(target_list): # Check if all targets have been recorded before.
-                        rep_list.append(target_list[target_idx]) # Append target where experimenter failed to repetition list.
+                    if target_idx < len(target_list):  # Check if all targets have been recorded before.
+                        rep_list.append(target_list[target_idx])  # Append target where experimenter failed to repetition list.
                     else: # If all targets have been recorded.
                         rep_list.append(rep_list[rep_idx])
 
@@ -366,23 +367,17 @@ def grasping_task(condition,prev):
             target_idx += 1
             # user_in
 
-            if target_idx > len(target_list)-1: # Check if all targets have been recorded.
+            if target_idx > len(target_list)-1:  # Check if all targets have been recorded.
                 if len(rep_list) != 0: # Check if targets must be repeated.
                     if len(rep_list) > rep_idx:
-                        print("Repetition target:" + str(rep_list[rep_idx])) 
-                        # print(rep_list)
-                        #rep_idx += 1
-                        
+                        print("Repetition target:" + str(rep_list[rep_idx]))
                     else: 
                         print("Block finished!")
                         return prev
                 else: 
                     print("Block finished!")
                     return prev
-            else: print(target_list[target_idx]) # Print target.                    
-        
-            #time.sleep(0.5)
-            #break
+            else: print(target_list[target_idx]) # Print target.
 
         elif keyboard.is_pressed('right') and not new_trial:
             curr = "r"
@@ -393,17 +388,15 @@ def grasping_task(condition,prev):
                 print("Right")
                 last = curr
 
-
-
         elif keyboard.is_pressed('left') and not new_trial:
             curr = "l"
             if curr != last:
                 if condition == "tactile":
                     belt_controller.vibrate_at_angle(45, channel_index=0, intensity=vibration_intensity)
                 num_instructions += 1
-                print("Left")   
+                print("Left")
                 last = curr
-            
+
 
         elif keyboard.is_pressed('down') and not new_trial:
             curr = "d"
@@ -413,7 +406,6 @@ def grasping_task(condition,prev):
                 num_instructions += 1
                 print("Down")
                 last = curr
-                
 
         elif keyboard.is_pressed('up') and not new_trial:
             curr = "u"
@@ -423,7 +415,6 @@ def grasping_task(condition,prev):
                 num_instructions += 1
                 print("Up")
                 last = curr
-
 
         elif keyboard.is_pressed('G') and not new_trial:
             curr = "f"
@@ -447,6 +438,7 @@ def grasping_task(condition,prev):
                 print("Grasp")
                 last = curr
 
+# the function to present example stimuli
 def present_example_stimuli(condition):
 
     curr = ""
@@ -454,7 +446,6 @@ def present_example_stimuli(condition):
 
     sample_targets = random.sample(list(range(1,10)), 3) # Draw 3 random targets
     print("Example targets: " + str(sample_targets))
-
 
     while True:
         # Stop the stimulus.
@@ -478,7 +469,7 @@ def present_example_stimuli(condition):
                     belt_controller.vibrate_at_angle(90, channel_index=0, intensity=vibration_intensity)
                 print("Up")
                 last = curr
-        #Present down stimulus.
+        # Present down stimulus.
         elif keyboard.is_pressed('down'):
             curr = "d"
             if curr != last:
@@ -523,9 +514,10 @@ def present_example_stimuli(condition):
                 belt_controller.stop_vibration()
             break
 
+
 def write_to_csv(id, observations, task):
 
-    #if file exists read rows into list so that new obs can be added to old
+    # if file exists read rows into list so that new obs can be added to old
     id = id
     obs = observations
     if task == "localization":
@@ -545,6 +537,7 @@ def write_to_csv(id, observations, task):
         for list in obs:
             writer.writerow(list)
 
+
 def connect_belt():
     setup_logger()
 
@@ -557,10 +550,11 @@ def connect_belt():
     # Change belt mode to APP mode
     belt_controller.set_belt_mode(BeltMode.APP_MODE)
 
+
 def collect_response():
     time_post_stim = time.perf_counter()
     response = ""
-    while time.perf_counter() - time_post_stim < 3: # Listen to keyboard input for 3 seconds.
+    while time.perf_counter() - time_post_stim < 3:  # Listen to keyboard input for 3 seconds.
         if keyboard.is_pressed("left"):
             response = "left"
         if keyboard.is_pressed("right"):
@@ -573,10 +567,12 @@ def collect_response():
         response = "no response"
     print(response)
     return response
-        
+
+
 def generate_participantID():
     participantID = str(str(randrange(10)) + str(randrange(10)) + str(randrange(10)) + str(randrange(10)))
     return participantID
+
 
 # Function to get user input taken from stack overflow
 def get_input(prompt, type_=None, min_=None, max_=None, range_=None):
@@ -611,13 +607,14 @@ def get_input(prompt, type_=None, min_=None, max_=None, range_=None):
         else:
             return ui
 
+
 def calc_accuracy():
     correct = 0
     num_blocks = 0
     mean_correct = 0
 
-    ### accuracy for first block
-    if len(obs_localization) == 1: # Check if at least 3 blocks were recorded
+    # accuracy for first block
+    if len(obs_localization) == 1:  # Check if at least 3 blocks were recorded
         for i in range(len(obs_localization[0])):    
             if obs_localization[0][i] == presented_stimuli_localization[0][i]:
                 correct += 1 
@@ -627,9 +624,8 @@ def calc_accuracy():
             mean_correct = correct / (num_blocks * len(obs_localization[0]))
     
         print(str(mean_correct) + "in 1st block")  
-    
 
-    ### accuracy for second block
+    # accuracy for second block
     if len(obs_localization) == 2: # Check if at least 3 blocks were recorded
         for i in range(len(obs_localization[1])):    
             if obs_localization[1][i] == presented_stimuli_localization[1][i]:
@@ -638,13 +634,10 @@ def calc_accuracy():
 
         if num_blocks > 0:
             mean_correct = correct / (num_blocks * len(obs_localization[0]))
-
-    
         print(str(mean_correct) + "in 2nd block")
 
-
-    if len(obs_localization) >= 3: # Check if at least 3 blocks were recorded
-        for x in range(-3,0): # Go through the last 3 blocks
+    if len(obs_localization) >= 3:  # Check if at least 3 blocks were recorded
+        for x in range(-3,0):  # Go through the last 3 blocks
             for i in range(len(obs_localization[x])):    
                 if obs_localization[x][i] == presented_stimuli_localization[x][i]:
                     correct += 1 
