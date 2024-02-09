34ece1a2375005f1804166403ee155029ad76f30
MiladRouygari <miladrouygari@gmail.com>
2024-02-09 10:42:44 +0100

    Clicker updated to work with presentation remote

--- aibox/clicker.py
+++ aibox/clicker.py
@@ -1,20 +1,21 @@
-from pynput.mouse import Button, Listener
+from pynput.keyboard import Key, Listener
 
-def on_click(x, y, button):
-    # Check if the pressed button is the right mouse button
-    if button == Button.left:
+def on_press(key):
+    # Check if the pressed key is the right arrow key
+    if key == Key.page_down:
         print("Next slide please!")
         # Implement what happens after the clicker button is pressed
         # Modify this to the master.py script 
 
-    # Check if the pressed button is the left mouse button to stop the listener
-    if button == Button.right:
+def on_release(key):
+    # Check if the pressed key is the escape key
+    if key == Key.esc:
+        # Stop the listener
         return False
 
-# Set up the listener for mouse events
-with Listener(on_click=on_click) as listener:
+# Set up the listener for keyboard events
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
-
 
         
 
