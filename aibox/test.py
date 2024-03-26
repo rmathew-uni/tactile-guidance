97a9b2750112a6fb71eeccac49b67ef078bde6ed
pippowell <93034705+pippowell@users.noreply.github.com>
2024-03-26 11:04:56 +0100

    Delete aibox/test.py

--- aibox/test.py
+++
@@ -1,9 +0,0 @@
-from pynput.mouse import Listener
-
-def on_click(x, y, button, pressed):
-    if pressed:
-        print(f"Mouse clicked at (x, y): {x}, {y}")
-        return False
-
-with Listener(on_click=on_click) as mouse_listener:
-    mouse_listener.join()
