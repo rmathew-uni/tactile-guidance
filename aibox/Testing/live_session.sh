44298c8bce774fd632f7c6bbc0a74445f74d93fb
Florian Pätzold <fpaetzold@uni-osnabrueck.de>
2024-04-02 12:18:18 +0200

    cleaning

--- aibox/Testing/live_session.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python HPC/optivist/ultralytics/detect.py --weights HPC/optivist/ultralytics/runs/train/exp8/weights/best.pt --source 0 --data 
-../OptiVisT/aibox/datasets/cocohands/cocohands.yaml --view-img
--- aibox/Testing/testing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 ../Training/optivist/ultralytics/detect.py --weights ../Training/optivist/ultralytics/runs/train/5sHands/weights/best.pt --source /Users/florian/Documents/Studium/NBP/Projects/OptiVisT/AIBox/Testing
