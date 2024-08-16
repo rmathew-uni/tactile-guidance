613f388d32eac815f69c9cf15e87cf0cb718217e
BwanaKumbwa <marcin.furtak2@gmail.com>
2024-08-16 10:40:48 +0200

    remove interactive belt connect

--- Shape_detection/calibration_training.py
+++ Shape_detection/calibration_training.py
@@ -7,14 +7,13 @@ import keyboard
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
-from openpyxl.workbook import Workbook
 from auto_connect import interactive_belt_connect, setup_logger
 from pybelt.belt_controller import (BeltConnectionState, BeltController,
                                     BeltControllerDelegate, BeltMode,
                                     BeltOrientationType,
                                     BeltVibrationTimerOption, BeltVibrationPattern)
 from pybelt.belt_scanner import BeltScanner
-
+from openpyxl.workbook import Workbook
 from bracelet import connect_belt
 
 connection_check, belt_controller = connect_belt()
@@ -23,77 +22,6 @@ if connection_check:
 else:
     print('Error connecting bracelet. Aborting.')
     sys.exit()
-
-def interactive_belt_connect(belt_controller):
-    """Interactive procedure to connect a belt. The interface to use is asked via the console.
-
-    :param BeltController belt_controller: The belt controller to connect.
-    """
-
-    # Ask for the interface
-    interface = input("Connect via Bluetooth or USB? [b,u]")
-    interface = ""
-    print("Connect via Bluetooth or USB? [b,u]", end="")
-    while interface == "":
-        interface = input()
-
-    if interface.lower() == "b":
-        # Scan for advertising belt
-        with pybelt.belt_scanner.create() as scanner:
-            print("Start BLE scan.")
-            belts = scanner.scan()
-            print("BLE scan completed.")
-        if len(belts) == 0:
-            print("No belt found.")
-            return belt_controller
-        if len(belts) > 1:
-            print("Select the belt to connect.")
-            for i, belt in enumerate(belts):
-                print("{}. {} - {}".format((i + 1), belt.name, belt.address))
-            belt_selection = input("[1-{}]".format(len(belts)))
-            try:
-                belt_selection_int = int(belt_selection)
-            except ValueError:
-                print("Unrecognized input.")
-                return belt_controller
-            print("Connect the belt.")
-            belt_controller.connect(belts[belt_selection_int - 1])
-        else:
-            print("Connect the belt.")
-            belt_controller.connect(belts[0])
-
-    elif interface.lower() == "u":
-        # List serial COM ports
-        ports = serial.tools.list_ports.comports()
-        if ports is None or len(ports) == 0:
-            print("No serial port found.")
-            return belt_controller
-        if len(ports) == 1:
-            connect_ack = 'y'
-            if connect_ack.lower() == "y" or connect_ack.lower() == "yes":
-                print("Connect the belt.")
-                belt_controller.connect(ports[0][0])
-            else:
-                print("Unrecognized input.")
-                return belt_controller
-        else:
-            print("Select the serial COM port to use.")
-            for i, port in enumerate(ports):
-                print("{}. {}".format((i + 1), port[0]))
-            belt_selection = input("[1-{}]".format(len(ports)))
-            try:
-                belt_selection_int = int(belt_selection)
-            except ValueError:
-                print("Unrecognized input.")
-                return belt_controller
-            print("Connect the belt.")
-            belt_controller.connect(ports[belt_selection_int - 1][0])
-
-    else:
-        print("Unrecognized input.")
-        return belt_controller
-
-    return belt_controller
 
 # Calibration function to determine optimal vibration intensity
 def calibrate_intensity():
@@ -287,15 +215,9 @@ def training_task():
     blocks = 3
     trials_per_block = 16
     block_accuracies = []
-
-    block_results = {}
-    combined_results = {
-        'Trial' : [],
-        'Block' : [],
-        'Actual Direction' : [],
-        'Predicted Direction' : [],
-        'Response Time (s)' : []
-    }
+    actual_directions =[]
+    predicted_directions = []
+    response_times = []
 
     for block in range(blocks):
         correct_responses = 0
@@ -305,13 +227,8 @@ def training_task():
         block_directions = directions * 2
         random.shuffle(block_directions)
 
-        actual_directions =[]
-        predicted_directions = []
-        response_times = []
-        trial_numbers = []
-
-        for trial_num, direction in enumerate(block_directions[:trials_per_block], start = 1):
-            print(f"Trial {trial_num}: Vibration direction is {direction}.")
+        for direction in block_directions[:trials_per_block]:
+            print(f"Trial: Vibration direction is {direction}.")
             vibrate_direction(direction)
             start_time = time.time()
             user_response = capture_direction()
@@ -321,29 +238,12 @@ def training_task():
             print(f"User response: {user_response}")
             belt_controller.stop_vibration()
             time.sleep(1)
-
-            trial_numbers.append(trial_num)
             actual_directions.append(direction)
             predicted_directions.append(user_response)
             response_times.append(response_time)
 
-            # Add to combined results
-            combined_results['Trial'].append(trial_num)
-            combined_results['Block'].append(block + 1)
-            combined_results['Actual Direction'].append(direction)
-            combined_results['Predicted Direction'].append(user_response)
-            combined_results['Response Time (s)'].append(response_time)
-
             if user_response == direction:
                 correct_responses += 1
-
-            # Store the results for the current block
-            block_results[f'Block {block + 1}'] = {
-                'Trial': trial_numbers,
-                'Actual Direction': actual_directions, 
-                'Predicted Direction': predicted_directions,
-                'Response Time (s)': response_times
-            }   
 
         # Stop vibration after completing a block with custom stop signal
         if belt_controller:
@@ -381,8 +281,7 @@ def training_task():
 
 
     # Save result to .txt file
-#    file_path = r"C:/Users/feelspace/OptiVisT/tactile-guidance/Shape_detection/training_result.txt"
-    file_path = r"D:/WWU/M8 - Master Thesis/Project/Code/training_result.txt"
+    file_path = r"C:/Users/feelspace/OptiVisT/tactile-guidance/Shape_detection/training_result.txt"
     with open(file_path, 'w') as file:  
         file.write(f"Selected intensity after training: {calibrated_intensity}\n")
         file.write(f"Block accuracy: {block_accuracies}\n")
@@ -390,47 +289,14 @@ def training_task():
     print('\nResults saved to training_result.txt')
 
     # Excel output
-#    with pd.ExcelWriter('C:/Users/feelspace/OptiVisT/tactile-guidance/Shape_detection/training_result.xlsx') as writer:
-    with pd.ExcelWriter('D:/WWU/M8 - Master Thesis/Project/Code/training_result.xlsx') as writer:
-        # Write the combined results to the first sheet
-        combined_df = pd.DataFrame(combined_results)
-        combined_df.to_excel(writer, sheet_name='All Blocks', index=False)
-        
-        # Write each block's results to subsequent sheets
-        for block_name, data in block_results.items():
-            df = pd.DataFrame(data)
-            df.to_excel(writer, sheet_name=block_name, index=False)
-    
-    #result = {'Actual Direction': actual_directions,
-    #     'Predicted Direction': predicted_directions,
-    #     'Response Time (s)': response_times}
-    #df = pd.DataFrame(data=result)
-    #df.to_excel("C:/Users/feelspace/OptiVisT/tactile-guidance/Shape_detection/training_result.xlsx")
+    result = {'Actual Direction': actual_directions,
+         'Predicted Direction': predicted_directions,
+         'Response Time (s)': response_times}
+    df = pd.DataFrame(data=result)
+    df.to_excel("C:/Users/feelspace/OptiVisT/tactile-guidance/Shape_detection/training_result.xlsx")
 
     return
     #return average_accuracy, block_accuracies, actual_directions, predicted_directions
-
-
-def visualize_confusion_matrix(excel_file_path):
-    # Load the first sheet from the Excel file
-    df = pd.read_excel(excel_file_path, sheet_name='All Blocks')
-
-    # Extract the actual and predicted directions
-    actual_directions = df['Actual Direction']
-    predicted_directions = df['Predicted Direction']
-
-    # Compute the confusion matrix
-    cm = confusion_matrix(actual_directions, predicted_directions)
-
-    # Plot the confusion matrix using Seaborn
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False,
-                xticklabels=df['Actual Direction'].unique(),
-                yticklabels=df['Actual Direction'].unique())
-    plt.xlabel('Predicted Direction')
-    plt.ylabel('Actual Direction')
-    plt.title('Confusion Matrix of Actual vs. Predicted Directions')
-    plt.show()
 
 
 # Run familiarization phase
@@ -439,9 +305,6 @@ def visualize_confusion_matrix(excel_file_path):
 # Run training task
 #training_accuracy = training_task()
 training_task()
-
-# Run confusion matrix
-visualize_confusion_matrix('D:/WWU/M8 - Master Thesis/Project/Code/training_result.xlsx')
 
 belt_controller.disconnect_belt() if belt_controller else None
 sys.exit()
