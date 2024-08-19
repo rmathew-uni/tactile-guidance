3030067aa98350aaff04a89f735a6ab2750b9de9
Felicia <feliciaelvina06@gmail.com>
2024-08-19 11:34:54 +0200

    change directions and add arrow directions

--- Shape_detection/visualization.py
+++ Shape_detection/visualization.py
@@ -4,7 +4,7 @@ import os
 
 # Define shapes with vertices
 shapes = {
-    '0': [(0, 0), (0, 4), (2, 4), (2, 0), (0, 0)],
+    '0': [(0, 0), (2, 0), (2, -4), (0, -4),  (0, 0)],
     '1': [(0, 0), (0, -2)],
     '2': [(0, 0), (2, 0), (2, -2), (0, -2), (0, -4), (2, -4)],
     '3': [(0, 0), (2, 0), (2, -2), (0, -2), (2, -2), (2, -4), (0, -4)],
@@ -12,33 +12,33 @@ shapes = {
     '5': [(0, 0), (-2, 0), (-2, -2), (0, -2), (0, -4), (-2, -4)],
     '6': [(0, 0), (-2, 0), (-2, -4), (0, -4), (0, -2), (-2, -2)],
     '7': [(0, 0), (2, 0), (2, -4)],
-    '8': [(0, 0), (-2, 0), (-2, -2), (0, -2), (0, -4), (-2, -4), (-2, -2), (0, -2), (0, 0)],
+    '8': [(0, 0), (2, 0), (2, -2), (0, -2), (0, -4), (2, -4), (2, -2), (0, -2), (0, 0)],
     '9': [(0, 0), (-2, 0), (-2, -2), (0, -2), (0, 0), (0, -4), (-2, -4)],
-    'a': [(0, 0), (-2, 0), (-2, -2), (0, -2), (0, 0), (0, -2.5)],
-    'b': [(0, 0), (0, -4), (2, -4), (2, -2), (0, -2)],
+    'a': [(0, 0), (-2, 0), (-2, 2), (0, 2), (0, -0.2)],
+    'b': [(0, 0), (2, 0), (2, -2), (0, -2), (0, 2)],
     'c': [(0, 0), (-2, 0), (-2, -2), (0, -2)],
-    'd': [(0, 0), (0, -4), (-2, -4), (-2, -2), (0, -2)],
+    'd': [(0, 0), (-2, 0), (-2, -2), (0, -2), (0, 2)],
     'e': [(0, 0), (2, 0), (2, 2), (0, 2), (0, -2), (2, -2)],
     'f': [(0, 0), (-2, 0), (-2, -4), (-2, -2), (0, -2)],
     'h': [(0, 0), (0, -4), (0, -2), (2, -2), (2, -4)],
-    'i': [(0, 0), (4, 0), (2, 0), (2, -4), (0, -4), (4, -4)],
+    'i': [(0, 0), (2, 0), (1, 0), (1, -4), (0, -4), (2, -4)],
     'j': [(0, 0), (2, 0), (2, -4), (0, -4), (0, -2)],
     'k': [(0, 0), (0, -4), (2, -2), (1, -3), (2, -4)],
     'l': [(0, 0), (0, -4), (2, -4)],
     'm': [(0, 0), (0, 4), (2, 2), (4, 4), (4, 0)],
     'n': [(0, 0), (0, 4), (2, 0), (2, 4)],
-    'p': [(0, 0), (0, 4), (2, 4), (2, 2), (0, 2)],
-    'q': [(0, 0), (0, 4), (-2, 4), (-2, 2), (0, 2)],
+    'p': [(0, 0), (2, 0), (2, 2), (0, 2), (0, -2)],
+    'q': [(0, 0), (-2, 0), (-2, 2), (0, 2), (0, -2)],
     'u': [(0, 0), (0, -2), (2, -2), (2, 0)],
-    'r': [(0, 0), (0, 4), (2, 4), (2, 2), (0, 2), (2, 0)],
+    'r': [(0, 0), (2, 0), (2, 2), (0, 2), (0, -2), (0,0), (2,-2)],
     'v': [(0, 0), (2, -4), (4, 0)],
     'w': [(0, 0), (0, -4), (2, -2), (4, -4), (4, 0)],
-    'y': [(0, 0), (2, -2), (4, 0), (0, -4)],
+    'y': [(0, 0), (2, -2), (4, 0), (2, -2), (2, -4)],
     'z': [(0, 0), (2, 0), (0, -2), (2, -2)]
 }
 
-# Function to visualize and save shapes
-def visualize_and_save_shapes(shapes, save_path, speed=1):
+# Function to visualize and save shapes with arrows
+def visualize_and_save_shapes_with_arrows(shapes, save_path):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
@@ -46,11 +46,13 @@ def visualize_and_save_shapes(shapes, save_path, speed=1):
         plt.figure()
         plt.axis('off')  # Turn off the axis
         
-        vertices.append(vertices[-1])  # Add the last vertex again to complete the shape
         for i in range(len(vertices) - 1):
             start = vertices[i]
             end = vertices[i + 1]
             plt.plot([start[0], end[0]], [start[1], end[1]], marker='o')
+            # with arrow
+            #plt.arrow(start[0], start[1], end[0] - start[0], end[1] - start[1],
+            #          head_width=0.2, length_includes_head=True, color='blue')
 
         plt.gca().set_aspect('equal', adjustable='box')
         
@@ -61,5 +63,5 @@ def visualize_and_save_shapes(shapes, save_path, speed=1):
 # Path to save the figures
 save_path = r"D:\WWU\M8 - Master Thesis\Project\Code\Images"
 
-# Visualize and save all shapes
-visualize_and_save_shapes(shapes, save_path)
+# Visualize and save all shapes with arrows
+visualize_and_save_shapes_with_arrows(shapes, save_path)
